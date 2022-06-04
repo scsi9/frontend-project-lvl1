8efbe361431f03c205a346da8902812984dad0a3
scsi9 <azat599115@gmail.com>
2022-06-04 21:25:39 +0300

    welcome

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
-//#!/usr/bin/env node
+import readlineSync from 'readline-sync'
 console.log('Welcome to the Brain Games!')
+const name = readlineSync.question('Your name? ');
+console.log('Hello' + ' ' + name)
